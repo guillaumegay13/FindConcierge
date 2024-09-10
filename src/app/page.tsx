359152ee1a0dac9d56a3ffabb27bbd7cda915f3f
Guillaume Gay <guillaume.gay@protonmail.com>
2024-09-10 10:52:30 +0200

    fix issue with Nextjs 13+

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import { redirect } from 'next/navigation';
+import { headers } from 'next/headers';
 import { DEFAULT_LANGUAGE } from './config';
 
-export default function Home({ headers }: { headers: Headers }) {
-  const host = headers.get('host') || '';
+export default function Home() {
+  const headersList = headers();
+  const host = headersList.get('host') || '';
+
   if (host.endsWith('.fr')) {
     redirect('/fr');
   } else if (host.endsWith('.app') || host.endsWith('.com')) {
