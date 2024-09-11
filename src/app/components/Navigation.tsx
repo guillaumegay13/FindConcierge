658194c736b39d38b221a971be2ed5fc46324506
Guillaume Gay <guillaume.gay@protonmail.com>
2024-09-11 07:32:26 +0200

    test new config

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import ThemeToggle from './ThemeToggle'
 
 type NavigationProps = {
-    // lang: string
+    lang: string
     dict: {
         home: string
         search: string
@@ -12,13 +12,13 @@ type NavigationProps = {
     }
 }
 
-export default function Navigation({ dict }: NavigationProps) {
+export default function Navigation({ lang, dict }: NavigationProps) {
     return (
         <nav className="bg-blue-500 p-4 flex justify-between items-center">
             <ul className="flex space-x-4 text-white">
-                <li><Link href={`/`}>{dict.home}</Link></li>
-                <li><Link href={`/search`}>{dict.search}</Link></li>
-                <li><Link href={`/register`}>{dict.registerConcierge}</Link></li>
+                <li><Link href={`/${lang}`}>{dict.home}</Link></li>
+                <li><Link href={`/${lang}search`}>{dict.search}</Link></li>
+                <li><Link href={`/${lang}/register`}>{dict.registerConcierge}</Link></li>
             </ul>
             <ThemeToggle />
         </nav>
